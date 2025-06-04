3b2a475972e0ffbb890826645d822a02adf2a0b4
Wellyson Carlos Pontes da Silva <wellyson.silva@cialne.com.br>
2025-06-04 16:38:30 -0300

    FEAT: Configurado mapeamento de servidor para rodar com swagger

--- src/main/java/br/com/lsimteste/pagamentos/PagamentosApplication.java
+++ src/main/java/br/com/lsimteste/pagamentos/PagamentosApplication.java
@@ -5,9 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@OpenAPIDefinition(
+    servers = { @Server(url = "http://localhost:8762", description = "Gateway Server URL") },
+    info = @Info(
+        title = "Pagamentos API",
+        version = "1.0.0",
+        description = "API de Pagamentos do Sistema LSIM")
+)
 public class PagamentosApplication {
 
 	public static void main(String[] args) {
